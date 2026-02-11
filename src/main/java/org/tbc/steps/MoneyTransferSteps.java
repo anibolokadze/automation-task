27bd879c32bf2933f9ba561e86d2075471bf3537
ABolokadze <anibolokadze@gmail.com>
2026-02-11 16:28:21 +0400

    test commit for a new branch

--- src/main/java/org/tbc/steps/MoneyTransferSteps.java
+++ src/main/java/org/tbc/steps/MoneyTransferSteps.java
@@ -34,6 +34,7 @@ public class MoneyTransferSteps extends MoneyTransferPage {
     public MoneyTransferSteps verifyCalculationResult(){
         calculationResult.scrollIntoViewIfNeeded();
         assertThat(calculationResult).isVisible();
+        //
         return this;
     }
     public MoneyTransferSteps verifyCalculationResultValue(){
